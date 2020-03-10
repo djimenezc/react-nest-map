fe3d82d7b2b9df40279763e0d08d22a041b96b3e
David Jimenez <david.jimenez19@gmail.com>
2020-03-11 00:05:37 +0100

    display map (google maps) using OpenLayers

--- packages/frontend/src/Map/Map.tsx
+++ packages/frontend/src/Map/Map.tsx
@@ -1,8 +1,37 @@
-import React from "react";
+import React, {useRef, useState} from "react";
 import {DevicesProps} from '../react-app-env'
 
+import OlMap from 'ol/Map';
+import OlView from 'ol/View';
+import OlLayerTile from 'ol/layer/Tile';
+// import OlSourceOSM from 'ol/source/OSM';
+import OlSourceXYZ from 'ol/source/XYZ';
+
 const Map: React.FC<DevicesProps> = ({devices}) => {
-    return (<div>map</div>)
+    const [center, setCenter] = useState([0, 0]);
+    const [zoom, setZoom] = useState(3);
+    const mapRef = useRef(null);
+
+    const olMap = new OlMap({
+        layers: [
+            new OlLayerTile({
+                source: new OlSourceXYZ({
+                    url: 'http://mt0.google.com/vt/lyrs=p&hl=en&x={x}&y={y}&z={z}'
+                })
+            })
+        ],
+        view: new OlView({
+            center: center,
+            zoom: zoom
+        })
+    });
+    olMap.setTarget(mapRef.current || '');
+
+    return (
+        <div ref={mapRef}
+             style={{width: "100%", height: "100%", position: 'absolute'}}>
+        </div>
+    )
 };
 
 export default Map;
