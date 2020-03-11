92e42ff64ba2439a62a0a36a2352a0aac8158e8d
David Jimenez <david.jimenez19@gmail.com>
2020-03-11 23:02:14 +0100

    center map and enable drag and wheel control

--- packages/frontend/src/Map/Map.tsx
+++ packages/frontend/src/Map/Map.tsx
@@ -4,33 +4,58 @@ import {DevicesProps} from '../react-app-env'
 import OlMap from 'ol/Map';
 import OlView from 'ol/View';
 import OlLayerTile from 'ol/layer/Tile';
-// import OlSourceOSM from 'ol/source/OSM';
+import TileLayer from 'ol/layer/Tile';
+import OlSourceOSM from 'ol/source/OSM';
 import OlSourceXYZ from 'ol/source/XYZ';
+import 'ol/ol.css';
+import {
+    defaults as defaultInteractions,
+    DragRotateAndZoom
+} from 'ol/interaction';
+import {defaults as defaultControls} from 'ol/control';
 
 const Map: React.FC<DevicesProps> = ({devices}) => {
     const [center, setCenter] = useState([0, 0]);
     const [zoom, setZoom] = useState(3);
     const mapRef = useRef(null);
+    const osm = new TileLayer({
+        source: new OlSourceOSM()
+    });
+    const googleMaps = new OlLayerTile({
+        source: new OlSourceXYZ({
+            url: 'http://mt0.google.com/vt/lyrs=p&hl=en&x={x}&y={y}&z={z}'
+        })
+    });
 
-    const olMap = new OlMap({
+    const map = new OlMap({
+        interactions: defaultInteractions().extend([
+            new DragRotateAndZoom()
+        ]),
+        controls: defaultControls(),
         layers: [
-            new OlLayerTile({
-                source: new OlSourceXYZ({
-                    url: 'http://mt0.google.com/vt/lyrs=p&hl=en&x={x}&y={y}&z={z}'
-                })
-            })
+            googleMaps,
+            // osm
         ],
         view: new OlView({
             center: center,
             zoom: zoom
         })
     });
-    olMap.setTarget(mapRef.current || '');
+    map.setTarget(mapRef.current || '');
 
     return (
-        <div ref={mapRef}
-             style={{width: "100%", height: "100%", position: 'absolute'}}>
+        <div style={{
+            padding: "0 2rem"
+        }}>
+            <div ref={mapRef}
+                 style={{
+                     width: "calc(100% - 4rem)",
+                     height: "90%",
+                     position: 'fixed'
+                 }}>
+            </div>
         </div>
+
     )
 };
 
